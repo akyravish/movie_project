c751649b43ec39ae1753f642013fe6f21416e2e9
akyravish <info.akshayravish@gmail.com>
2021-05-21 08:30:21 +0530

    add further code to manupulate the dom

--- app.js
+++ app.js
@@ -7,14 +7,48 @@ const SEARCH_API =
 	'https://api.themoviedb.org/3/search/movie?api_key=c03300aa3d24442048373ae91cec8a3c&query="';
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 getApiData(API_URL);
+
+const colorRating = (vote) => {
+	if (vote >= 8) {
+		return 'green';
+	} else if (vote >= 5) {
+		return 'orange';
+	} else {
+		return 'red';
+	}
+};
+
+const getMovie = (movies) => {
+	movies.forEach((movie) => {
+		const { title, vote_average, poster_path, overview } = movie;
+		const movieEL = document.createElement('div');
+		movieEL.classList.add('movie');
+		movieEL.innerHTML = `
+            <img src="${IMAGE + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${colorRating(
+					vote_average
+				)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `;
+
+		main.appendChild(movieEL);
+	});
+};
 
 async function getApiData(url) {
 	const res = await fetch(url);
 	const data = await res.json();
 
-	console.log(data.results);
+	getMovie(data.results);
 }
 
 form.addEventListener('submit', (e) => {
